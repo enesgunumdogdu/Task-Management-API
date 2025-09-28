a4bb8c0bb2599554e9c548cbcc4acdadf42a606b
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-09-28 15:28:05 +0300

    Dtos created: TaskDto.java, TaskListDto.java

---
+++ src/main/java/com/enesgunumdogdu/tasks/domain/dto/TaskDto.java
@@ -0,0 +1,17 @@
+package com.enesgunumdogdu.tasks.domain.dto;
+
+import com.enesgunumdogdu.tasks.domain.entities.TaskPriority;
+import com.enesgunumdogdu.tasks.domain.entities.TaskStatus;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record TaskDto(
+        UUID uuid,
+        String title,
+        String description,
+        LocalDateTime dueDate,
+        TaskPriority priority,
+        TaskStatus status
+) {
+}
