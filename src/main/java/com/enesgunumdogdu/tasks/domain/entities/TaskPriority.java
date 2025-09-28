fe45d78f3b271bd3d0d2c9edadc13d44fdcbcfda
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-09-28 14:12:52 +0300

    Enums added for tasks.

---
+++ src/main/java/com/enesgunumdogdu/tasks/domain/entities/TaskPriority.java
@@ -0,0 +1,5 @@
+package com.enesgunumdogdu.tasks.domain.entities;
+
+public enum TaskPriority {
+    HIGH, MEDIUM, LOW
+}
---
+++ src/main/java/com/enesgunumdogdu/tasks/domain/entities/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.enesgunumdogdu.tasks.domain.entities;
+
+public enum TaskStatus {
+    OPEN,CLOSED
+}
