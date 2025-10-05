000a283f28482c6654e52bd6a2a54dc83d630af3
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-10-05 14:27:06 +0300

    Add transactional support to updateTaskList and updateTask methods in TaskListServiceImpl and TaskServiceImpl for improved data integrity during updates.

--- src/main/java/com/enesgunumdogdu/tasks/services/impl/TaskListServiceImpl.java
+++ src/main/java/com/enesgunumdogdu/tasks/services/impl/TaskListServiceImpl.java
@@ -3,6 +3,7 @@ package com.enesgunumdogdu.tasks.services.impl;
 import com.enesgunumdogdu.tasks.domain.entities.TaskList;
 import com.enesgunumdogdu.tasks.repositories.TaskListRepository;
 import com.enesgunumdogdu.tasks.services.TaskListService;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -52,6 +53,7 @@ public class TaskListServiceImpl implements TaskListService {
         return taskListRepository.findById(id);
     }
 
+    @Transactional
     @Override
     public TaskList updateTaskList(UUID taskListId, TaskList taskList) {
         if(null ==  taskList.getId()) {
