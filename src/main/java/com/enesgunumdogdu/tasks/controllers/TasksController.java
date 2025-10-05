3c2bd548f053aae7f4f03ebae572615e41330683
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-10-05 13:56:18 +0300

    Add createTask endpoint to TasksController and implement task creation logic in TaskService and TaskServiceImpl

--- src/main/java/com/enesgunumdogdu/tasks/controllers/TasksController.java
+++ src/main/java/com/enesgunumdogdu/tasks/controllers/TasksController.java
@@ -1,12 +1,10 @@
 package com.enesgunumdogdu.tasks.controllers;
 
 import com.enesgunumdogdu.tasks.domain.dto.TaskDto;
+import com.enesgunumdogdu.tasks.domain.entities.Task;
 import com.enesgunumdogdu.tasks.mappers.TaskMapper;
 import com.enesgunumdogdu.tasks.services.TaskService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -29,5 +27,11 @@ public class TasksController {
                 .stream()
                 .map(taskMapper::toDto)
                 .toList();
+    }
+
+    @PostMapping
+    public TaskDto createTask(@PathVariable("task_list_id") UUID taskListId, @RequestBody TaskDto taskDto) {
+        Task createdTask = taskService.createTask(taskListId,taskMapper.fromDto(taskDto));
+        return taskMapper.toDto(createdTask);
     }
 }
--- src/main/java/com/enesgunumdogdu/tasks/services/impl/TaskServiceImpl.java
+++ src/main/java/com/enesgunumdogdu/tasks/services/impl/TaskServiceImpl.java
@@ -1,25 +1,68 @@
 package com.enesgunumdogdu.tasks.services.impl;
 
 import com.enesgunumdogdu.tasks.domain.entities.Task;
+import com.enesgunumdogdu.tasks.domain.entities.TaskList;
+import com.enesgunumdogdu.tasks.domain.entities.TaskPriority;
+import com.enesgunumdogdu.tasks.domain.entities.TaskStatus;
+import com.enesgunumdogdu.tasks.repositories.TaskListRepository;
 import com.enesgunumdogdu.tasks.repositories.TaskRepository;
 import com.enesgunumdogdu.tasks.services.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
+    private final TaskListRepository taskListRepository;
 
-    public TaskServiceImpl(TaskRepository taskRepository) {
+    public TaskServiceImpl(TaskRepository taskRepository, TaskListRepository taskListRepository) {
         this.taskRepository = taskRepository;
+        this.taskListRepository = taskListRepository;
     }
 
 
     @Override
     public List<Task> listTasks(UUID taskListId) {
         return taskRepository.findByTaskListId(taskListId);
+    }
+
+    @Override
+    public Task createTask(UUID taskListId, Task task) {
+        if(null != task.getId()){
+            throw new IllegalArgumentException("Task already has an ID!");
+        }
+
+        if(null == task.getTitle() || task.getTitle().isBlank()){
+            throw new IllegalArgumentException("Task must have a title!");
+        }
+
+        TaskPriority taskPriority = Optional.ofNullable(task.getPriority())
+                .orElse(TaskPriority.MEDIUM);
+
+        TaskStatus taskStatus = TaskStatus.OPEN;
+
+        TaskList taskList = taskListRepository.findById(taskListId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Task List ID provided!"));
+
+
+        LocalDateTime now =  LocalDateTime.now();
+        Task taskToSave = new Task(
+                null,
+                task.getTitle(),
+                task.getDescription(),
+                task.getDueDate(),
+                taskStatus,
+                taskPriority,
+                taskList,
+                now,
+                now
+        );
+
+        return taskRepository.save(taskToSave);
     }
 }
